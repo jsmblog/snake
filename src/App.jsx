adbac84aae7dbb97b19f396398d1a46d193efd50
joel <jsmblog19@gmail.com>
2023-06-23 22:29:04 -0500

    fix img

--- src/App.jsx
+++ src/App.jsx
@@ -215,9 +215,9 @@ const App = () => {
         </div>
       </div>
       <div className="direction-buttons">
+        <button onClick={() => handleButtonClick('left')}> <span className='Arrows Arrows_left'><img width={40} src={arrow} alt="" /></span></button>
         <button onClick={() => handleButtonClick('up')}> <span className='Arrows Arrows_up '><img width={40} src={arrow} alt="" /></span></button>
         <button onClick={() => handleButtonClick('down')}> <span className='Arrows Arrows_down'><img width={40} src={arrow} alt="" /></span></button>
-        <button onClick={() => handleButtonClick('left')}> <span className='Arrows Arrows_left'><img width={40} src={arrow} alt="" /></span></button>
         <button onClick={() => handleButtonClick('right')}> <span className='Arrows Arrows_right'><img width={40} src={arrow} alt="" /></span></button>
       </div>
       {gameOver && (
