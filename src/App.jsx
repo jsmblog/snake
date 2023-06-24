55d7d35f5389331eb22bab1e4078f5b38556bdf7
joel <jsmblog19@gmail.com>
2023-06-24 00:14:59 -0500

    sound bomb and bombs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import soundPrincipal from '/sound-principal.mp3';
 import useSound from 'use-sound';
 import soundEating from '/sound-eating.mp3';
 import arrow from '../src/img/arrow.png';
+import soundExplosion from '/explosion.mp3';
+
+
 const GRID_SIZE = 20;
 const INITIAL_SNAKE = [
   { x: 10, y: 10 },
@@ -14,7 +17,7 @@ const COLORS = ['blue', 'green', 'purple', 'gold', 'yellow', 'red', 'violet', 'D
 const SCORE_MULTIPLIERS = [2, 4, 6, 8, 10, 2, 4, 6, 8, 2, 4, 6, 8, 10, 2, 4, 6, 8, 10];
 const SPEED_INCREMENT = 5;
 const INITIAL_SPEED = 203;
-const APPLE_DURATION = 2380;
+const APPLE_DURATION = 2400;
 const APPLE_INTERVAL = 40000;
 
 const App = () => {
@@ -27,7 +30,8 @@ const App = () => {
 
   const [playEating] = useSound(soundEating, { volume: 10 });
   const [playPrincipal, { stop }] = useSound(soundPrincipal, { volume: 1, loop: true });
-
+  const [bombs, setBombs] = useState([]);
+  const [playExplosion] = useSound(soundExplosion, { volume: 1 });
   const [snake, setSnake] = useState(INITIAL_SNAKE);
   const [direction, setDirection] = useState(INITIAL_DIRECTION);
   const [apple, setApple] = useState(generateRandomApple());
@@ -68,6 +72,26 @@ const App = () => {
     };
   }, [direction]);
 
+  const generateRandomBomb = () => {
+    const randomX = Math.floor(Math.random() * GRID_SIZE);
+    const randomY = Math.floor(Math.random() * GRID_SIZE);
+    return { x: randomX, y: randomY };
+  };
+  useEffect(() => {
+    const bombInterval = setInterval(() => {
+      const newBombs = [];
+      for (let i = 0; i < 5; i++) {
+        newBombs.push(generateRandomBomb());
+      }
+      setBombs(newBombs);
+    }, 60000);
+  
+    return () => {
+      clearInterval(bombInterval);
+    };
+  }, []);
+    
+
   useEffect(() => {
     const moveSnake = setInterval(() => {
       setSnake((prevSnake) => {
@@ -92,11 +116,13 @@ const App = () => {
           newHead.x < 0 ||
           newHead.x >= GRID_SIZE ||
           newHead.y < 0 ||
-          newHead.y >= GRID_SIZE
+          newHead.y >= GRID_SIZE ||
+          bombs.some((bomb) => bomb.x === newHead.x && bomb.y === newHead.y)
         ) {
           clearInterval(moveSnake);
           setGameOver(true);
-          return newSnake; // Return the current snake without modifying it
+          playExplosion(); // Reproducir el efecto de sonido de explosión
+          return newSnake; // Devolver la serpiente actual sin modificarla
         }
 
         if (newSnake.slice(1).some((segment) => segment.x === newHead.x && segment.y === newHead.y)) {
@@ -183,35 +209,54 @@ const App = () => {
   const handleButtonClick = (newDirection) => {
     setDirection(newDirection);
   };
+  
+  useEffect(() => {
+    const generateBombs = () => {
+      const newBombs = [];
+      for (let i = 0; i < 10; i++) {
+        newBombs.push(generateRandomBomb());
+      }
+      setBombs(newBombs);
+    };
+  
+    const bombInterval = setInterval(generateBombs, 30000);
+    generateBombs(); // Generar bombas inmediatamente
+  
+    return () => {
+      clearInterval(bombInterval);
+    };
+  }, []);
+  
 
   return (
     <div className="game-container">
       <h1 className='titleGame'>Snake Game</h1>
       <div className='CardGame'>
         <div className="grid">
-          {Array.from({ length: GRID_SIZE }).map((_, rowIndex) => (
-            <div key={rowIndex} className="row">
-              {Array.from({ length: GRID_SIZE }).map((_, colIndex) => {
-                const isSnakeHead = snake[0].x === colIndex && snake[0].y === rowIndex;
-                const isSnakeBody = snake.some(
-                  (segment, index) =>
-                    index > 0 && segment.x === colIndex && segment.y === rowIndex
-                );
-                const isApple = apple.x === colIndex && apple.y === rowIndex;
-                const appleColor = isApple ? apple.color : 'red';
+        {Array.from({ length: GRID_SIZE }).map((_, rowIndex) => (
+  <div key={rowIndex} className="row">
+    {Array.from({ length: GRID_SIZE }).map((_, colIndex) => {
+      const isSnakeHead = snake[0].x === colIndex && snake[0].y === rowIndex;
+      const isSnakeBody = snake.some(
+        (segment, index) => index > 0 && segment.x === colIndex && segment.y === rowIndex
+      );
+      const isApple = apple.x === colIndex && apple.y === rowIndex;
+      const isBomb = bombs.some((bomb) => bomb.x === colIndex && bomb.y === rowIndex);
+      const appleColor = isApple ? apple.color : 'red';
 
-                return (
-                  <div
-                    key={`${rowIndex}-${colIndex}`}
-                    className={`cell ${isSnakeHead ? 'snake-head' : ''} ${
-                      isSnakeBody ? 'snake-body' : ''
-                    } ${isApple ? 'apple' : ''}`}
-                    style={isApple ? { backgroundColor: appleColor } : null}
-                  />
-                );
-              })}
-            </div>
-          ))}
+      return (
+        <div
+          key={`${rowIndex}-${colIndex}`}
+          className={`cell ${isSnakeHead ? 'snake-head' : ''} ${
+            isSnakeBody ? 'snake-body' : ''
+          } ${isApple ? 'apple' : ''} ${isBomb ? 'bomb' : ''}`}
+          style={isApple ? { backgroundColor: appleColor } : null}
+        />
+      );
+    })}
+  </div>
+))}
+
         </div>
       </div>
       <div className="direction-buttons">
